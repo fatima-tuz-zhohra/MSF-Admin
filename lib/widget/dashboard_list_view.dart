169502a5bbed50b237a043ba5950dfd3bd9ad469
ftzelma <fatima.tuz.zhohra@gmail.com>
2021-09-27 00:11:56 +0600

    modify dashboard

--- lib/widget/dashboard_list_view.dart
+++ lib/widget/dashboard_list_view.dart
@@ -8,12 +8,16 @@ class DashboardListView extends StatelessWidget {
     required this.subTitle,
     required this.image,
     required this.onTap,
+    required this.color,
+    required this.color2,
   }) : super(key: key);
 
   String title;
   String subTitle;
   String image;
   Function() onTap;
+  Color color;
+  Color color2;
 
   @override
   Widget build(BuildContext context) {
@@ -29,13 +33,13 @@ class DashboardListView extends StatelessWidget {
           //padding: EdgeInsets.all(8),
           child: Stack(
             children: [
-              Positioned(top: 20, right: 12, child: Image.asset(image),
+              Positioned(top: 10, right: 10, child: Image.asset(image),
               height: 60,
               width: 60,),
               Container(
               padding: EdgeInsets.all(8),
                 width: 240,
-                color: Colors.lightBlue.withOpacity(0.8),
+                color: color.withOpacity(0.8),
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -53,7 +57,7 @@ class DashboardListView extends StatelessWidget {
                   width: 240,
                   height: 24,
                   padding: EdgeInsets.all(4),
-                  color: Colors.blue,
+                  color: color2,
                   child: Text('More Info', textAlign: TextAlign.center,
                   style: theme.textTheme.bodyText1?.copyWith(color: Colors.white, fontSize: 10),),
                 ),
