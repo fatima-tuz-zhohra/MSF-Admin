f13d0657e96fdf81aff9dccba5e0215fff44e505
ftzelma <fatima.tuz.zhohra@gmail.com>
2021-10-13 18:51:50 +0600

    add oxygen list

--- lib/common/data/services/database.dart
+++ lib/common/data/services/database.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_project_template/common/data/model/items/hospital_item.dart';
 import 'package:flutter_project_template/common/data/model/profile_data.dart';
-import 'package:flutter_project_template/feature/home/profile_screen.dart';
 
 class DatabaseService{
   final String uid;
@@ -80,5 +79,16 @@ class HospitalService {
       });
       return hospitals;
     });
+  }
+}
+
+class OxygenService {
+  OxygenService();
+
+  final CollectionReference oxygenCollection =
+  FirebaseFirestore.instance.collection('Oxygen_Supplier');
+
+  Stream<QuerySnapshot<Object?>> getOxygen() {
+    return oxygenCollection.snapshots();
   }
 }
--- lib/feature/oxygen/oxygen_supplier_screen.dart
+++ lib/feature/oxygen/oxygen_supplier_screen.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project_template/common/data/model/items/oxygen_item.dart';
+import 'package:flutter_project_template/common/data/services/database.dart';
+import 'package:flutter_project_template/widget/List_item.dart';
 import 'package:flutter_project_template/widget/msf_admin_base_page_layout.dart';
+import 'package:flutter_project_template/widget/top_bar.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class OxygenSupplierScreen extends StatefulWidget {
   const OxygenSupplierScreen({Key? key}) : super(key: key);
@@ -14,11 +20,102 @@ class _OxygenSupplierScreenState extends State<OxygenSupplierScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: TopBar(title: 'Oxygen Supplier List'),
       body: MsfAdminBasePageLayout(
-        child: CustomScrollView(
+        child:  StreamBuilder<QuerySnapshot<Object?>>(
+          stream: OxygenService().getOxygen(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Container(
+                height: 44,
+                width: 44,
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData) {
+              final data = snapshot.requireData;
+              final List<OxygenItem> oxygens = [];
 
+              data.docs.forEach((element) {
+                final dbItem = element.data()! as Map<String, dynamic>;
+                final oxygen = OxygenItem(
+                    dbItem['name'],
+                    dbItem['address'],
+                    dbItem['PhoneNo'],
+                    dbItem['latitude'],
+                    dbItem['longitude']);
+                oxygens.add(oxygen);
+              });
+              return OxygenListContent(oxygens: oxygens);
+            } else {
+              return Container();
+            }
+          },
         ),
       ),
     );
+  }
+}
+
+class OxygenListContent extends StatefulWidget {
+  const OxygenListContent({Key? key, required this.oxygens}) : super(key: key);
+  final List<OxygenItem> oxygens;
+
+  @override
+  _OxygenListContentState createState() => _OxygenListContentState();
+}
+
+class _OxygenListContentState extends State<OxygenListContent> {
+  List<OxygenItem> oxygens = [];
+  String searchKey = "";
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    if (searchKey.isEmpty) {
+      oxygens = widget.oxygens;
+    }
+    return ListView.builder(
+          itemCount: oxygens.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListItem(
+              child: ListTile(
+                title: Text('${oxygens[index].name}'),
+                subtitle: Text('${oxygens[index].address}'),
+                trailing: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      iconSize: 18,
+                      icon: const Icon(Icons.location_on),
+                      color: theme.colorScheme.secondary,
+                      onPressed: () {
+                        navigateTo(oxygens[index].latitude,
+                            oxygens[index].longitude);
+                      },
+                    ),
+                    IconButton(
+                      iconSize: 18,
+                      icon: const Icon(Icons.call),
+                      color: theme.colorScheme.secondary,
+                      onPressed: () {
+                        launch(('tel://${oxygens[index].phoneNo}'));
+                      },
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+    );
+  }
+
+  void navigateTo(double lat, double lng) async {
+    var uri = Uri.parse("http://maps.google.com/maps?daddr=$lat,$lng");
+    if (await canLaunch(uri.toString())) {
+      await launch(uri.toString());
+    } else {
+      throw 'Could not launch ${uri.toString()}';
+    }
   }
 }
