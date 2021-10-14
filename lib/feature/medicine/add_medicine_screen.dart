67392d782c8ce104ccb5d63b0d2c749cebe8eed6
ftzelma <fatima.tuz.zhohra@gmail.com>
2021-10-14 21:42:43 +0600

    add medicine submit button

--- lib/feature/medicine/add_medicine_screen.dart
+++ lib/feature/medicine/add_medicine_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_template/util/ui_utils.dart';
 import 'package:flutter_project_template/widget/add_input_field.dart';
 import 'package:flutter_project_template/widget/msf_admin_base_page_layout.dart';
+import 'package:flutter_project_template/widget/rounded_button.dart';
 import 'package:flutter_project_template/widget/top_bar.dart';
 
 class AddMedicineScreen extends StatelessWidget {
   static const ROUTE = "/add-new-medicine";
 
-   AddMedicineScreen({Key? key}) : super(key: key);
+  AddMedicineScreen({Key? key}) : super(key: key);
 
   final medicineNameController = TextEditingController();
   final medicineGenericController = TextEditingController();
@@ -17,42 +19,66 @@ class AddMedicineScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: TopBar(title: 'Add New Medicine Item',),
-
+      appBar: TopBar(
+        title: 'Add New Medicine Item',
+      ),
       body: MsfAdminBasePageLayout(
         child: CustomScrollView(
           slivers: [
-           SliverFillRemaining(
-             hasScrollBody: false,
-               child: _buildBody(context),
-           ),
+            SliverFillRemaining(
+              hasScrollBody: false,
+              child: _buildBody(context),
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget _buildBody(BuildContext context){
+  Widget _buildBody(BuildContext context) {
     return Form(
       child: Column(
         children: [
-          SizedBox(height: 16,),
-          AddInputField(hintText: 'Medicine Name', controller: medicineNameController),
-          SizedBox(height: 12,),
-
-          AddInputField(hintText: 'Medicine Generic', controller: medicineGenericController),
-          SizedBox(height: 12,),
-
-          AddInputField(hintText: 'Company Name', controller: medicineCompanyNameController),
-          SizedBox(height: 12,),
-
-          AddInputField(hintText: 'Medicine Price', controller: medicinePriceController),
-          SizedBox(height: 12,),
-
-          AddInputField(hintText: 'Medicine Description', controller: medicineDescriptionController),
-          SizedBox(height: 12,),
+          SizedBox(
+            height: 16,
+          ),
+          AddInputField(
+              hintText: 'Medicine Name', controller: medicineNameController),
+          SizedBox(
+            height: 12,
+          ),
+          AddInputField(
+              hintText: 'Medicine Generic',
+              controller: medicineGenericController),
+          SizedBox(
+            height: 12,
+          ),
+          AddInputField(
+              hintText: 'Company Name',
+              controller: medicineCompanyNameController),
+          SizedBox(
+            height: 12,
+          ),
+          AddInputField(
+              hintText: 'Medicine Price', controller: medicinePriceController),
+          SizedBox(
+            height: 12,
+          ),
+          AddInputField(
+              hintText: 'Medicine Description',
+              controller: medicineDescriptionController),
+          SizedBox(
+            height: 20,
+          ),
+          RoundedButton(text: 'Submit Medicine',
+              press: () {
+            /*if()  {}
+            else{
+              showSnackbar(context, Text('Invalid Inputs'),);
+            }*/
+              },)
         ],
-      ), );
+      ),
+    );
   }
-
 }
