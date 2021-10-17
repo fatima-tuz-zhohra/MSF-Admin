28fc42dc8a205604dd1defb14b4d8b5dbe4cdf0e
ftzelma <fatima.tuz.zhohra@gmail.com>
2021-10-17 18:49:42 +0600

    modify button menu

--- lib/feature/dashboard_main/main_navigation.dart
+++ lib/feature/dashboard_main/main_navigation.dart
@@ -15,133 +15,183 @@ class MainNavigation extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TopBar(title: 'Menu',canGoBack: false,),
-          ProfileScreen(),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TopBar(title: 'Menu',canGoBack: false,),
+            ProfileScreen(),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Main Navigation'),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      textStyle: theme.textTheme.subtitle2,
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, DashboardScreen.ROUTE);
+                    },
+                    child:  Row(
+                      children: [
+                        Icon(Icons.dashboard),
+                        SizedBox(width: 8,),
+                        Text('Dashboard'),
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      textStyle: theme.textTheme.subtitle2,
+                    ),
+                    onPressed: () {},
+                    child:  Row(
+                      children: [
+                        Icon(Icons.category),
+                        SizedBox(width: 8,),
+                        Text('Category'),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                     padding:  EdgeInsets.only(left:52.0),
+                     child: SizedBox(
+                         child: Column(
+                           crossAxisAlignment: CrossAxisAlignment.start,
+                           children: [
+                             TextButton(
+                               style: TextButton.styleFrom(
+                                 padding: const EdgeInsets.all(16.0),
+                                 textStyle: theme.textTheme.bodyText2,
+                               ),
+                               onPressed: () {
+                                 Navigator.pushNamed(context, MedicineScreen.ROUTE);
+                               },
+                               child:  Text('Medicines'),
+                             ),
+                             TextButton(
+                               style: TextButton.styleFrom(
+                                 padding: const EdgeInsets.all(16.0),
+                                 textStyle: theme.textTheme.bodyText2,
+                               ),
+                               onPressed: () {
+                                 Navigator.pushNamed(context, HospitalScreen.ROUTE);
+                               },
+                               child:  Text('Hospitals'),
+                             ),
+                             TextButton(
+                               style: TextButton.styleFrom(
+                                 padding: const EdgeInsets.all(16.0),
+                                 textStyle: theme.textTheme.bodyText2,
+                               ),
+                               onPressed: () {
+                                 Navigator.pushNamed(context, DoctorScreen.ROUTE);
+                               },
+                               child:  Text('Doctors'),
+                             ),
+                             TextButton(
+                               style: TextButton.styleFrom(
+                                 padding: const EdgeInsets.all(16.0),
+                                 textStyle: theme.textTheme.bodyText2,
+                               ),
+                               onPressed: () {
+                                 Navigator.pushNamed(context, BloodDonorScreen.ROUTE);
+                               },
+                               child: const Text('Blood Donors'),
+                             ),
+                             TextButton(
+                               style: TextButton.styleFrom(
+                                 padding:  EdgeInsets.all(16.0),
+                                 textStyle: theme.textTheme.bodyText2,
+                               ),
+                               onPressed: () {
+                                 Navigator.pushNamed(context, OxygenSupplierScreen.ROUTE);
+                               },
+                               child:  Text('Oxygen Suppliers'),
+                             ),
+                           ],
+                         )),
+                   ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      textStyle: theme.textTheme.subtitle2,
+                    ),
+                    onPressed: () {},
+                    child:  Row(
+                      children: [
+                        Icon(Icons.chat),
+                        SizedBox(width: 8,),
+                        Text('Chatting'),
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      textStyle: theme.textTheme.subtitle2,
+                    ),
+                    onPressed: () {},
+                    child:  Row(
+                      children: [
+                        Icon(Icons.notifications),
+                        SizedBox(width: 8,),
+                        Text('Send Notification'),
+                      ],
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      textStyle: theme.textTheme.subtitle2,
+                    ),
+                    onPressed: () {},
+                    child:  Row(
+                      children: [
+                        Icon(Icons.reviews),
+                        SizedBox(width: 8,),
+                        Text('Reviews'),
+                      ],
+                    ),
 
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Main Navigation'),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    textStyle: theme.textTheme.subtitle2,
                   ),
-                  onPressed: () {
-                    Navigator.pushNamed(context, DashboardScreen.ROUTE);
-                  },
-                  child: const Text('Dashboard'),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    textStyle: theme.textTheme.subtitle2,
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      textStyle: theme.textTheme.subtitle2,
+                    ),
+                    onPressed: () {},
+                    child:  Row(
+                      children: [
+                        Icon(Icons.settings),
+                        SizedBox(width: 8,),
+                        Text('Settings'),
+                      ],
+                    ),
                   ),
-                  onPressed: () {},
-                  child: const Text('Category'),
-                ),
-                 Padding(
-                   padding: const EdgeInsets.only(left:24.0),
-                   child: SizedBox(
-                       child: Column(
-                         crossAxisAlignment: CrossAxisAlignment.start,
-                         children: [
-                           TextButton(
-                             style: TextButton.styleFrom(
-                               padding: const EdgeInsets.all(16.0),
-                               textStyle: theme.textTheme.bodyText2,
-                             ),
-                             onPressed: () {
-                               Navigator.pushNamed(context, MedicineScreen.ROUTE);
-                             },
-                             child: const Text('Medicines'),
-                           ),
-                           TextButton(
-                             style: TextButton.styleFrom(
-                               padding: const EdgeInsets.all(16.0),
-                               textStyle: theme.textTheme.bodyText2,
-                             ),
-                             onPressed: () {
-                               Navigator.pushNamed(context, HospitalScreen.ROUTE);
-                             },
-                             child: const Text('Hospitals'),
-                           ),
-                           TextButton(
-                             style: TextButton.styleFrom(
-                               padding: const EdgeInsets.all(16.0),
-                               textStyle: theme.textTheme.bodyText2,
-                             ),
-                             onPressed: () {
-                               Navigator.pushNamed(context, DoctorScreen.ROUTE);
-                             },
-                             child: const Text('Doctors'),
-                           ),
-                           TextButton(
-                             style: TextButton.styleFrom(
-                               padding: const EdgeInsets.all(16.0),
-                               textStyle: theme.textTheme.bodyText2,
-                             ),
-                             onPressed: () {
-                               Navigator.pushNamed(context, BloodDonorScreen.ROUTE);
-                             },
-                             child: const Text('Blood Donors'),
-                           ),
-                           TextButton(
-                             style: TextButton.styleFrom(
-                               padding: const EdgeInsets.all(16.0),
-                               textStyle: theme.textTheme.bodyText2,
-                             ),
-                             onPressed: () {
-                               Navigator.pushNamed(context, OxygenSupplierScreen.ROUTE);
-                             },
-                             child: const Text('Oxygen Suppliers'),
-                           ),
-                         ],
-                       )),
-                 ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    textStyle: theme.textTheme.subtitle2,
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      padding: const EdgeInsets.all(16.0),
+                      textStyle: theme.textTheme.subtitle2,
+                    ),
+                    onPressed: () {},
+                    child:  Row(
+                      children: [
+                        Icon(Icons.info),
+                        SizedBox(width: 8,),
+                        Text('About Us'),
+                      ],
+                    ),
                   ),
-                  onPressed: () {},
-                  child: const Text('Chatting'),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    textStyle: theme.textTheme.subtitle2,
-                  ),
-                  onPressed: () {},
-                  child: const Text('Send Notification'),
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    textStyle: theme.textTheme.subtitle2,
-                  ),
-                  onPressed: () {},
-                  child: const Text('Reviews'),
-
-                ),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.all(16.0),
-                    textStyle: theme.textTheme.subtitle2,
-                  ),
-                  onPressed: () {},
-                  child: const Text('Settings'),
-
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
